2d13a12388cfbf31b9d334f8a9d11dfb159c0717
tooyamk <0xff000000@gmail.com>
2017-10-10 21:27:59 +0800

    null

--- characters/Elementalist/scripts/action/Skill0.lua
+++ characters/Elementalist/scripts/action/Skill0.lua
@@ -2,7 +2,7 @@
 local C = registerClassAuto(getClass(ACTION_PACKAGE, ACTION_BASE));
 
 function C:isCancel(tag, itemPtr)
-	if tag == MAGIC_ACTION_INDEX then
+	if tag == CGameAction.ACTION_SKILL..MAGIC_ACTION_INDEX then
 		return true;
 	else
 		return false;
